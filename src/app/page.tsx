ab15a91e9adb2425614bf4d3fbf1cc27cc4fcb32
Ludovic Dumas <ludoloops@pm.me>
2023-05-23 13:45:22 -0400

    feat: add todo item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <h1>hi</h1>
+import { prisma } from "@/db";
+import { TodoItem } from "./components/TodoItem";
+import Link from "next/link";
+
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+export default async function Home() {
+  const todos = await getTodos();
+
+  return (
+    <>
+      <header className=" mb-4 flex items-center justify-between">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          href="/new"
+          className="rounded border border-slate-300 px-2 py-1 text-slate-300 focus-within:bg-slate-700 hover:bg-slate-700"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
+    </>
+  );
 }
