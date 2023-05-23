c5cd519829bfac271f4998c6f8606a8ee96fc6be
Ludovic Dumas <ludoloops@pm.me>
2023-05-23 14:40:38 -0400

    done

--- src/app/components/TodoItem.tsx
+++ src/app/components/TodoItem.tsx
@@ -1,13 +1,22 @@
+"use client"
+
 type TodoItemProps = {
   id: string
   title: string
-  complete: Boolean
+  complete: boolean
+  toggleTodo: (id: string, complete: boolean) => void
 }
 
-export function TodoItem({ id, title, complete }: TodoItemProps) {
+export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
   return (
     <li className="flex items-center gap-1">
-      <input id={id} type="checkbox" className="peer cursor-pointer" />
+      <input
+        id={id}
+        type="checkbox"
+        className="peer cursor-pointer"
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
       <label
         htmlFor={id}
         className="cursor-pointer peer-checked:text-slate-500 peer-checked:line-through"
